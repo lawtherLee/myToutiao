65cd902e0a924c5f25af6340dae99197ac13dd1f
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-01-01 22:25:57 +0800

    feat:首页搜索框布局

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="home-container">首页</div>
+  <div class="home-container">
+    <!-- 导航栏 -->
+    <van-nav-bar class="navbar">
+      <template #title>
+        <van-button
+          block
+          icon="search"
+          round
+          size="small"
+          @click="$router.push('/search')"
+        >搜索
+        </van-button>
+      </template>
+    </van-nav-bar>
+    <!-- /导航栏 -->
+  </div>
 </template>
 
 <script>
@@ -20,4 +35,81 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.navbar {
+  background-color: #3296fa;
+
+  // inherit 继承
+  // unset 不设置
+  :deep(.van-nav-bar__title) {
+    max-width: inherit;
+  }
+
+  .van-button--default {
+    background-color: #5babfb;
+    border: 0;
+    color: #fff;
+    font-size: 30px;
+  }
+
+  .van-icon {
+    color: #fff;
+  }
+
+  .van-button--block {
+    width: 7.4rem;
+  }
+}
+
+/* tabs导航条样式 */
+:deep(.van-tabs__wrap) {
+  padding-right: 66px;
+
+  .van-tabs__nav {
+    padding-left: 0;
+    padding-right: 0;
+
+    /* tab标签 */
+
+    .van-tab {
+      border: 1px solid #eee;
+      width: 200px;
+      height: 82px;
+    }
+
+    /* tab标签下划线 */
+
+    .van-tabs__line {
+      width: 31px;
+      height: 6px;
+      background-color: #3296fa;
+      bottom: 40px;
+    }
+  }
+}
+
+/* 字体图标 */
+.toutiao-gengduo {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 66px;
+  height: 82px;
+  font-size: 40px;
+  line-height: 82px;
+  text-align: center;
+  opacity: 0.6;
+  border-bottom: 1px solid #eee;
+
+  &::after {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 70%;
+    width: 1px;
+    background-image: url("~@/assets/images/gradient-gray-line.png");
+  }
+}
+</style>
